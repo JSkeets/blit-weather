0a35e864cbdf80b1441f6c34a63225e4986ecc3a
Jesse Skeets <jesseskeets@gmail.com>
2018-03-16 10:19:28 -0700

    Successfully retrieves weather from front-end

--- weatherapp/app/controllers/weather_controller.rb
+++ weatherapp/app/controllers/weather_controller.rb
@@ -1,15 +1,21 @@
 class WeatherController < ApplicationController
 
     def index
-        response = RestClient::Request.execute(
-            method: :get,
-            url: 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=4e66533961b500086bf6bd7c37d4b847'
-        )
+     
         render json: response
     end
 
     def create
-        p "Waffles";
+
+        url = "http://api.openweathermap.org/data/2.5/weather?zip=" + params[:zip] +"&APPID=4e66533961b500086bf6bd7c37d4b847"
+    
+
+ 
+        @weather = RestClient::Request.execute(
+            method: :get,
+            url: url
+        )
+        render json: @weather
     end
 
     def weather_params
--- weatherapp/client/src/App.js
+++ weatherapp/client/src/App.js
@@ -3,20 +3,30 @@ import logo from "./logo.svg";
 import "./App.css";
 import $ from "jquery";
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { zip: "" };
+    this.update = this.update.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
     let weather = $.ajax({
-      method: "GET",
-      url: `http://localhost:3001/weather`
+      method: "POST",
+      url: `http://localhost:3001/weather`,
+      data: { zip: this.state.zip }
     });
 
     console.log(weather);
+  }
+
+  update(field) {
+    return e =>
+      this.setState({
+        [field]: e.currentTarget.value
+      });
   }
 
   render() {
@@ -27,7 +37,15 @@ class App extends Component {
         <h1>HERE I AM MOtheR</h1>
         <h1>HERE I AM MOtheR</h1>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" />
+          <label>
+            Please Enter Your Zip Code
+            <input
+              type="text"
+              placeholder="12345"
+              value={this.state.zip}
+              onChange={this.update("zip")}
+            />
+          </label>
           <input type="submit" value="Submit" />>
         </form>
       </div>
